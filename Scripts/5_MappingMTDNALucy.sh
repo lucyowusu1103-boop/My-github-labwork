4aee5e03591fc98c60fc91b3df4a8c4ff4281cb7
Lucy O. Owusu <f008f6j@slurm-fe01-prd.dartmouth.edu>
2026-04-24 09:30:15 -0400

    Added mtdna mapping Lucy edited script

--- Scripts/5_MappingMTDNALucy.sh
+++ Scripts/5_MappingMTDNALucy.sh
@@ -32,14 +32,13 @@ SAMPLE="CAL03"
 BASE_PATH="/dartfs/rc/lab/F/FleskesR/BioinfoWG/USERS/${USER}"
 
 #make new directory for bwa mapping and samtools filtering output plus one for qualimap (Note: un-comment this if you have not yet made these directories)
-mkdir -p ${BASE_PATH}/Data/4_MappingMTDNA/mapped
-mkdir -p ${BASE_PATH}/Data/4_MappingMTDNA/qualimap
+# mkdir -p ${BASE_PATH}/Data/5_MappingMTDNA{mapped,qualimap}
 
 #specify paths (Note: you may need to change PATH_TRIMMED if you needed to run additional trimming to remove all adapters)
-PATH_TRIMMED="${BASE_PATH}/Data/2_AdapterRemoval/AdapterROutput"
-PATH_MAP="${BASE_PATH}/Data/4_MappingMTDNA/mapped"
-PATH_QUALI="${BASE_PATH}/Data/4_MappingMTDNA/qualimap"
-PATH_REF=/dartfs-hpc/rc/home/5/f008715/FleskesR/ReferenceSeqs/MtDNA
+PATH_TRIMMED="${BASE_PATH}/Data/2_AdapterRemoval/FINISHED"
+PATH_MAP="${BASE_PATH}/Data/5_MappingMTDNA/mapped"
+PATH_QUALI="${BASE_PATH}/Data/5_MappingMTDNA/qualimap"
+PATH_REF=/dartfs/rc/lab/F/FleskesR/ReferenceSeqs/MtDNA
 
 #move to the right folder
 cd $PATH_TRIMMED
@@ -53,9 +52,10 @@ echo -e "Sample \t Mapped Reads \t  % Mapped \t Q30 Mapped Reads \t Removed Dupl
 
 echo -e "BEGIN MAPPING"
 
-for j in *truncated.gz
+for j in *truncated.gz *.trimmed.fastq.gz *.trimmed2.fastq.gz *.trimmed3.fastq.gz  #Change file suffixes if you had to run other trimming programs
 do   
-    NAME=${j%.fastq.truncated.gz}  #Change ".fastq.truncated.gz" to correct file suffix if you had to run other trimming programs
+    [ -f "$j" ] || continue  #skip if no files with this suffix
+    NAME=${j%%.*}  #Change ".fastq.truncated.gz" to correct file suffix if you had to run other trimming programs
     echo "Running..."${NAME}""
 
     echo "Aligning ${NAME}.mtDNA"
